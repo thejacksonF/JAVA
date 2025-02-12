979bd52854d2d19581c71ec9cc507783e0ab6d7b
Wick <sehla.tqm.0316@gmail.com>
2025-02-12 11:06:59 -0600

    Le puse comentarios a la clase principal

--- zPrincipal.java
+++ zPrincipal.java
@@ -2,16 +2,18 @@ import java.util.Scanner;
 
 public class zPrincipal {
     public static void main(String[] args) {
-        
+        //CLASE JUEGO
        Juego obJuego= new Juego();
        obJuego.Piedra_papel_o_tijera();
-
+        //CLASE NOMBRES
       Nombres objNombres = new Nombres();
        objNombres.lista_de_nombres_de_los_estudiantes();
 
+        //CLASE PROMEDIO
        Promedio objPromedio = new Promedio();
         objPromedio.lista_de_calificaciones();
 
+        //CLASE USERDATA
         UserData objUserData = new UserData();
         String fullName = objUserData.NombreyApellido("Francisco", "Avitia");
         System.out.println(fullName);
@@ -25,14 +27,12 @@ public class zPrincipal {
 
         int finalGrade = objStudentsGrades.calculateFinalGrade (80,75,90,95);
         System.out.println(finalGrade);
-
-
         String percentageOfAbsences = objStudentsGrades.checkFailureByAbsences(64,7);
         System.out.println(percentageOfAbsences);
 
 
+        //Objeto del TriangleArea
         Scanner objScanner = new Scanner(System.in) ;
-        
         System.out.print("Dame el valor de la area: ");
         double base = objScanner.nextDouble();
 
