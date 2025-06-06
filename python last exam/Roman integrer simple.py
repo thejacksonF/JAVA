01e6aa08e457e1ff10f39251ae4123a452507a90
Wick <sehla.tqm.0316@gmail.com>
2025-06-05 19:53:52 -0600

    cambios

--- python last exam/Roman integrer simple.py
+++ python last exam/Roman integrer simple.py
@@ -1,46 +1,46 @@
 class Solution(object):
-    def CalcularValoresRomanos(self, s):
+    def CalcularValoresRomanos(self, valorEnTexto):
         Valores_de_las_letras = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
 
         resultado  = 0
         i = 0
-        while i < len(s):
-            if i + 1 < len(s):
-                LETTERS = s[i] + s[i + 1]
-                if LETTERS == 'IV':
+        while i < len(valorEnTexto):
+            if i + 1 < len(valorEnTexto):
+                LETTERS = valorEnTexto[i] + valorEnTexto[i + 1]
+                if LETTERS == "IV":
                     resultado += 4
                     i += 2
                     continue
-                elif LETTERS == 'IX':
+                elif LETTERS == "IX":
                     resultado += 9
                     i += 2
                     continue
-                elif LETTERS == 'XL':
+                elif LETTERS == "XL":
                     resultado += 40
                     i += 2
                     continue
-                elif LETTERS == 'XC':
+                elif LETTERS == "XC":
                     resultado += 90
                     i += 2
                     continue
-                elif LETTERS == 'CD':
+                elif LETTERS == "CD":
                     resultado += 400
                     i += 2
                     continue
-                elif LETTERS == 'CM':
+                elif LETTERS == "CM":
                     resultado += 900
                     i += 2
                     continue
 
-            resultado += Valores_de_las_letras[s[i]]
+            resultado += Valores_de_las_letras[valorEnTexto[i]]
             i += 1
 
         return resultado
